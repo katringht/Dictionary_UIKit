af1680a85ae129feb4e8639e98f00ae317c6b5a2
katringht <katrin_tko@mail.ru>
2021-02-11 15:01:54 +0300

    days 17, 18

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -10,15 +10,20 @@ import UIKit
 class ViewController: UITableViewController {
     
     var pictures = [String]()
+    var selectedPictureNumber = 0
+    var totalPictures = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "LightHouses View"
+        navigationController?.navigationBar.prefersLargeTitles = true
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
         
-        for item in items{
+        for item in items.sorted(){
             if item.hasPrefix("lighthouse"){
                 pictures.append(item)
             }
@@ -35,6 +40,16 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController{
+            vc.selectedImage = pictures[indexPath.row]
+            vc.selectedPictureNumber = indexPath.row + 1
+            vc.totalPictures = pictures.count
+            
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 
 }
