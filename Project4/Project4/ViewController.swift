4272d846ac3f771cb9a3989b5817e0cb7c1d2cda
katringht <katrin_tko@mail.ru>
2021-02-14 15:10:59 +0300

    day 24

---
+++ Project4/Project4/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Ekaterina Tarasova on 14.02.2021.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let url = URL(string: "https://github.com/katringht")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+    @objc func openTapped(){
+        let ac = UIAlertController(title: "Open page..", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "github.com/katringht", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cansel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction){
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+}
+
